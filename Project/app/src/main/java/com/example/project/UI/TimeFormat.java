ef2134c971a625735755f5c835d7247da49d1c7c
1753021 <51194229+1753021@users.noreply.github.com>
2020-06-05 02:27:24 +0700

    merge folder (error git)

---
+++ Project/app/src/main/java/com/example/project/UI/TimeFormat.java
@@ -0,0 +1,21 @@
+package com.example.project.UI;
+
+import java.util.Formatter;
+
+public class TimeFormat {
+    private static final int LARGE_TIME = 99 * 60000 +  59 * 1000;
+
+    private StringBuilder mTimeText;
+    private Formatter mTimeFormatter;
+
+    public String formatTime(long time) {
+        mTimeText = new StringBuilder();
+        mTimeFormatter = new Formatter(mTimeText);
+        if (time > LARGE_TIME) {
+            mTimeFormatter.format("%d:%02d", time / 60000, time / 1000 % 60);
+        } else {
+            mTimeFormatter.format("%02d:%02d", time / 60000, time / 1000 % 60);
+        }
+        return mTimeText.toString();
+    }
+}
